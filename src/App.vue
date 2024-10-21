b45694a2d206a7230920a3b7cdb6a4cf07bd49cb
Adebola <bola@neegles.com>
2024-10-21 11:54:03 +0100

    Tablink component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,39 @@
 <template>
-  <main>Hello</main>
+  <main class="max-w-2xl px-4 mx-auto">
+    <nav
+      class="mb-4 font-medium text-center text-gray-500 border-b border-gray-200"
+    >
+      <ul class="flex flex-wrap">
+        <li v-for="tab in tabs" :key="tab.key">
+          <TabLink :tab="tab" :currentTab="currentTab" />
+        </li>
+      </ul>
+    </nav>
+  </main>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { TabKey, Tab } from '@/types'
+
+import TabLink from '@/components/TabLink.vue'
+
+const tabs: Tab[] = [
+  {
+    key: 'General',
+    label: 'General',
+  },
+  {
+    key: 'Notifications',
+    label: 'Notifications',
+  },
+  {
+    key: 'Privacy',
+    label: 'Privacy',
+  },
+]
+
+const currentTab = ref<TabKey>('General')
+</script>
 
 <style scoped></style>
---
+++ src/components/TabLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>{{ tab }}</div>
+</template>
+
+<script setup lang="ts">
+import type { Tab } from '@/types'
+
+defineProps<{
+  currentTab: string
+  tab: Tab
+}>()
+
+defineEmits<{
+  click: []
+}>()
+</script>
+
+<style scoped></style>
